1e8a253399c3dbf062a600432f28a598de88de72
Imtiyaz Haider <imtiyazhaider11@gmail.com>
2026-04-09 13:31:30 +0530

    postgres connected

--- config.py
+++ config.py
@@ -10,3 +10,20 @@ class Config:
     # Image upload folder
     UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static', 'images')
     UPLOAD_TEMP = os.path.join(BASE_DIR, 'uploads')
+
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback_key')
+
+    uri = os.environ.get('DATABASE_URL')
+
+    # Fix for Render (sometimes gives postgres://)
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = uri or 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
