78f429c894772f48aae5a55435a1d0c9d5225db2
Imtiyaz Haider <imtiyazhaider11@gmail.com>
2026-04-23 03:06:20 +0530

    added wishlist page

--- routes/products.py
+++ routes/products.py
@@ -3,3 +3,22 @@ def home():
     from models import Product
     products = Product.query.all()
     return render_template('home.html', products=products)
+
+from models import Wishlist
+from flask_login import current_user
+
+@app.route('/')
+def home():
+    products = Product.query.all()
+
+    wishlist_items = []
+    if current_user.is_authenticated:
+        wishlist_items = [
+            item.product_id for item in Wishlist.query.filter_by(user_id=current_user.id).all()
+        ]
+
+    return render_template(
+        'home.html',
+        products=products,
+        wishlist_items=wishlist_items
+    )
--- routes/wishlist.py
+++ routes/wishlist.py
@@ -1,10 +1,28 @@
-from flask import Blueprint, redirect, request
+from flask import Blueprint, render_template, redirect, request
 from flask_login import login_required, current_user
-from models import Wishlist
+from models import Wishlist, Product
 from extensions import db
 
 wishlist_bp = Blueprint('wishlist', __name__)
 
+
+# ❤️ VIEW WISHLIST
+@wishlist_bp.route('/wishlist')
+@login_required
+def view_wishlist():
+
+    items = Wishlist.query.filter_by(user_id=current_user.id).all()
+
+    products = []
+    for item in items:
+        product = Product.query.get(item.product_id)
+        if product:
+            products.append(product)
+
+    return render_template('wishlist.html', products=products)
+
+
+# 🔄 TOGGLE (already exists)
 @wishlist_bp.route('/wishlist/toggle/<int:product_id>')
 @login_required
 def toggle_wishlist(product_id):
